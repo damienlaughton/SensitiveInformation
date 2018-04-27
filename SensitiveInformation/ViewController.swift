d4f729c570120fc286746d253dd78ad485dd7de8
Damo <dfplaughton@googlemail.com>
2018-04-27 10:45:25 +0100

    Initial Commit

---
+++ SensitiveInformation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SensitiveInformation
+//
+//  Created by Damien Laughton on 27/04/2018.
+//  Copyright © 2018 Mobilology Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
